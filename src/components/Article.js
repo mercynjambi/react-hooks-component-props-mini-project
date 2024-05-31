f08e947efaf2e395bf317b9dd652e6b50d8f1505
mercynjambi <njambim695@gmail.com>
2024-05-31 13:14:37 +0300

    updated the 'minute to read indicator'

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,13 +1,20 @@
-import React from 'react'
+// src/components/Article.js
+import React from 'react';
 
-function Article({title, date ='January 1, 1970', preview}) {
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+  const coffeeCup = "☕️";
+  const bentoBox = "🍱";
+  const minutesToRead = minutes < 30
+    ? coffeeCup.repeat(Math.ceil(minutes / 5))
+    : bentoBox.repeat(Math.ceil(minutes / 10));
+
   return (
     <article>
-        <h3>{title}</h3>
-        <small>{date}</small>
-        <p>{preview}</p>
+      <h3>{title}</h3>
+      <small>{date} • {minutesToRead} {minutes} min read</small>
+      <p>{preview}</p>
     </article>
-  )
+  );
 }
 
-export default Article
+export default Article;
