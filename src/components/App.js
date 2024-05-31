f75ff6d40392062ad5e8ea4ee7b5583391754ef3
mercynjambi <njambim695@gmail.com>
2024-05-31 12:13:29 +0300

    created the header and about section

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import Header from "../components/Header";
 import blogData from "../data/blog";
+import About from "../components/About";
 
-console.log(blogData);
+// console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      {/* <p>hey</p> */}
+      <Header name={blogData.name}/>
+      <About image={blogData.image} about={blogData.about} />
     </div>
   );
 }
